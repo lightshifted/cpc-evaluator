c1c5c0b23d3c9e8b8d1a76b7241b491032b9dcff
masslightsquared <jason.wheeler@aapc.com>
2024-07-28 14:00:05 -0400

    tidy code

--- utils/query_handler.py
+++ utils/query_handler.py
@@ -1,20 +1,20 @@
 import configparser
 import json
 import os
-import yaml
 from typing import Any, Dict, List, Tuple, Union
-from dotenv import load_dotenv
+
 import openai
+import yaml
+from dotenv import load_dotenv
 
 load_dotenv()
+
 
 class QueryHandler:
     def __init__(self):
         self.config = configparser.ConfigParser()
         self.config.read("./config.ini")
-        self.openai_client = openai.OpenAI(
-            api_key=os.getenv("OPENAI_API_KEY")
-        )
+        self.openai_client = openai.OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
     def get_openai_completion(
         self,
@@ -22,8 +22,7 @@ class QueryHandler:
     ) -> Dict[str, Any]:
         model_name = self.config.get("LLM", "openai_model")
         response = self.openai_client.chat.completions.create(
-            model=model_name, 
-            messages=[{"role": "user", "content": prompt}]
+            model=model_name, messages=[{"role": "user", "content": prompt}]
         )
         return json.loads(response.model_dump_json())
 
@@ -32,24 +31,31 @@ class QueryHandler:
             return yaml.load(file, Loader=yaml.FullLoader)
 
     def query(
-            self,
-            query: str,
-            provider: str="openai",
+        self,
+        query: str,
+        provider: str = "openai",
     ):
         if provider == "openai":
-            prompt = self._load_prompt(
-                self.config.get("LLM", "clinical_phrase_prompt"))\
-                    .get("template").format(query=query)
+            prompt = (
+                self._load_prompt(self.config.get("LLM", "clinical_phrase_prompt"))
+                .get("template")
+                .format(query=query)
+            )
 
-            response = self.get_openai_completion(prompt)\
-                .get("choices")[0].get("message").get("content")
+            response = (
+                self.get_openai_completion(prompt)
+                .get("choices")[0]
+                .get("message")
+                .get("content")
+            )
 
             return [{"content": response}]
+
 
 if __name__ == "__main__":
     sh = QueryHandler()
     clinical_phrase_example = "She presents for pre-op risk assessment. Scheduled for removal of a lipoma from the right arm. She has no cardiorespiratory symptoms of significance. She has a past medical history significant for a post-op CVA detected 2 weeks after a uterine fibroid embolization in 2018. She had a pulmonary embolus after a laparoscopic sleeve gastrectomy in 2021. She has normal exercise capacity of greater than 4 METs and no symptoms with a normal EKG."
     result = sh.query(
-        query=clinical_phrase_example, 
+        query=clinical_phrase_example,
     )
     print(result)
